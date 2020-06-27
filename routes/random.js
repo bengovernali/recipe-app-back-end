b354e2be2e2b1e1a4ce9b3aaecf6b70792f3518f
Ben Governali <bgovern93@gmail.com>
2020-06-27 13:07:14 -0400

    completed random recipe route, added cors, fetch, and dotenv dependencies.

--- routes/random.js
+++ routes/random.js
@@ -1,23 +1,22 @@
-const express = require("express"),
-  cors = require("cors"),
-  fetch = require("node-fetch");
+const express = require("express");
+const fetch = require("node-fetch");
+
+require("dotenv").config();
 
 const router = express.Router();
 
-const API_KEY = process.env["SPOONACULAR_KEY"];
-
-router.use(cors());
+const API_KEY = process.env.SPOONACULAR_KEY;
 
 async function getRandomRecipe() {
     const response = await fetch(
         `https://api.spoonacular.com/recipes/random?apiKey=${API_KEY}`
     )
-    console.log(response);
+    return response.json();
 }
 
-router.get("/random", async function(req, res) {
+router.get("/", async function(req, res) {
     const recipe = await getRandomRecipe();
-    return recipe;
+    res.send(recipe);
 }) 
 
 module.exports = router;

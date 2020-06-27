7d26a3574ab590f9118eaa6a7ca1b294e39a5268
Ben Governali <bgovern93@gmail.com>
2020-06-27 11:39:05 -0400

    create initial random route setup

--- routes/random.js
+++ routes/random.js
@@ -4,4 +4,20 @@ const express = require("express"),
 
 const router = express.Router();
 
+const API_KEY = process.env["SPOONACULAR_KEY"];
+
 router.use(cors());
+
+async function getRandomRecipe() {
+    const response = await fetch(
+        `https://api.spoonacular.com/recipes/random?apiKey=${API_KEY}`
+    )
+    console.log(response);
+}
+
+router.get("/random", async function(req, res) {
+    const recipe = await getRandomRecipe();
+    return recipe;
+}) 
+
+module.exports = router;
